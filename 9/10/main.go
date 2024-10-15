8946e66fcce400294ad610f681b59bd27bdc90d4
jaennil <jaennil@yandex.ru>
2024-10-15 20:44:02 +0300

    feat(9): task 10

---
+++ 9/10/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/IBM/sarama"
+)
+
+var brokers = []string{"localhost:9092"}
+
+func main() {
+    config := sarama.NewConfig()
+    config.Producer.Return.Successes = true
+    config.Producer.Return.Errors = true
+
+    topic := "test-topic"
+    group := "test-group"
+
+    admin, err := sarama.NewClusterAdmin(brokers, config)
+    if err != nil {
+        log.Fatalf("error while creating cluster admin: %v", err)
+    }
+    defer admin.Close()
+
+    err = admin.CreateTopic(topic, &sarama.TopicDetail{
+        NumPartitions:     3,
+        ReplicationFactor: 1,
+    }, false)
+    if err != nil {
+        if errors.Is(err, sarama.ErrTopicAlreadyExists) {
+            log.Println("topic already exists")
+        } else {
+            log.Fatalf("error while creating topic: %v", err)
+        }
+    } else {
+        log.Println("INFO: topic created")
+    }
+
+    go produceMessagesAsync(topic)
+
+    consumeMessagesWithGroup(topic, group)
+}
+
+func NewAsyncProducer() (sarama.AsyncProducer, error) {
+    config := sarama.NewConfig()
+    config.Producer.Return.Successes = true
+    config.Producer.Return.Errors = true
+    producer, err := sarama.NewAsyncProducer(brokers, config)
+    return producer, err
+}
+
+func produceMessagesAsync(topic string) {
+    producer, err := NewAsyncProducer()
+    if err != nil {
+        log.Fatalf("error while creating producer: %v", err)
+    }
+    defer producer.AsyncClose()
+
+    go func() {
+        for success := range producer.Successes() {
+            log.Printf("sent message to partition %d with offset %d with key %s", success.Partition, success.Offset, success.Key)
+        }
+    }()
+
+    go func() {
+        for err := range producer.Errors() {
+            log.Printf("error while sending message: %v", err)
+        }
+    }()
+
+    for i := 0; i < 3; i++ {
+        for j := 0; j < 10; j++ {
+            message := fmt.Sprintf("Message %d", j)
+            key := fmt.Sprintf("%d_%d", i, j)
+            producer.Input() <- &sarama.ProducerMessage{
+                Topic: topic,
+                Key: sarama.StringEncoder(key),
+                Partition: int32(i),
+                Value: sarama.StringEncoder(message),
+            }
+
+            log.Printf("message sent: %s", message)
+        }
+    }
+}
+
+type ConsumerGroupHandler struct {
+    ready chan bool
+}
+
+func (h *ConsumerGroupHandler) Setup(sarama.ConsumerGroupSession) error  {
+    close(h.ready)
+    return nil
+}
+
+func (h *ConsumerGroupHandler) Cleanup(sarama.ConsumerGroupSession) error {
+    return nil
+}
+
+func (h * ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession,  claim sarama.ConsumerGroupClaim) error {
+    for message := range claim.Messages() {
+        log.Printf("got message: topic=%s partition=%d offset=%d key=%s: %s", message.Topic, message.Partition, message.Offset, message.Key, string(message.Value))
+    }
+    return nil
+}
+
+func consumeMessagesWithGroup(topic string, group string) {
+    consumer := ConsumerGroupHandler {
+        ready: make(chan bool),
+    }
+
+    config := sarama.NewConfig()
+    config.Consumer.Return.Errors = true
+    config.Consumer.Offsets.Initial = sarama.OffsetOldest
+    config.Consumer.Group.Rebalance.Strategy = sarama.NewBalanceStrategyRoundRobin()
+
+    client, err := sarama.NewConsumerGroup(brokers, group, config)
+    if err != nil {
+        log.Fatalf("error while creating consumer group: %v", err)
+    }
+    defer client.Close()
+
+    go func() {
+        for err := range client.Errors() {
+            log.Printf("error in consumer group: %v", err)
+        }
+    }()
+
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    go func() {
+        for {
+            err := client.Consume(ctx, []string{topic}, &consumer)
+            if err != nil {
+                log.Printf("error while consuming: %v", err)
+            }
+
+            if ctx.Err() != nil {
+                return
+            }
+            consumer.ready = make(chan bool)
+        }
+    }()
+
+    <- consumer.ready
+    log.Println("consumer ready and joined the group")
+
+    sigterm := make(chan os.Signal, 1)
+    signal.Notify(sigterm, os.Interrupt)
+    <-sigterm
+    log.Println("got os interrupt. exiting")
+    log.Println("done")
+}
